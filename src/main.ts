904a0281cd562db226529d67c54d4e26e34503b2
karman07 <84911866+karman07@users.noreply.github.com>
2025-01-31 13:34:12 +0530

    backend done

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//import { VercelAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: 'https://frontend-nrygc84lh-karman07s-projects.vercel.app',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Make sure OPTIONS is included
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
